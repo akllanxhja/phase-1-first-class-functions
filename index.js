4ecb4caf0b31fc6bedf2f388db678a5cc9721798
AlexanderKllanxhja <akllanxhja@gmail.com>
2021-07-15 15:59:36 -0600

    Almost done

--- index.js
+++ index.js
@@ -1,75 +1,114 @@
-function Monday() {
-    console.log('Go for a five-mile run');
-    console.log('Pump iron');
-  }
-  
-  function Tuesday() {
-    console.log('Go for a five-mile run');
-    console.log('Swim 40 laps');
-  }
-  
-  function Wednesday() {
-    console.log('Go for a five-mile run');
-    console.log('Go for a five-mile run');
-  }
-  
-  function Thursday() {
-    console.log('Go for a five-mile run');
-    console.log('Pump iron');
-  }
-  
-  function Friday() {
-    console.log('Go for a five-mile run');
-    console.log('Swim 40 laps');
-  }
-  
-  function runFiveMiles() {
-    console.log('Go for a five-mile run');
-  }
+// function Monday() {
+//   console.log('Go for a five-mile run');
+//   console.log('Pump iron');
+// }
 
-  function liftWeights() {
-    console.log('Pump iron');
-  }
-  
-  function swimFortyLaps() {
-    console.log('Swim 40 laps');
-  }
+// function Tuesday() {
+//   console.log('Go for a five-mile run')
+//   console.log('Swim 40 laps');
+// }
 
-  function Monday() {
-    runFiveMiles();
-    liftWeights();
-  }
+// function Wednesday() {
+//   console.log('Go for a five-mile run');
+//   console.log('Go for a five-mile run');
+// }
 
-  function exerciseRoutine(postRunActivity) {
-    runFiveMiles();
-    postRunActivity();
-  }
+// function Thursday() {
+//   console.log('Go for a five-mile run');
+//   console.log('Pump iron');
+// }
 
-  function Monday() {
-    exerciseRoutine(liftWeights);
-  }
+// function Friday() {
+//   console.log('Go for a five-mile run');
+//   console.log('Swim 40 laps');
+// }
 
-  exerciseRoutine(() => console.log('Stretch! Work that core!'));
+// function runFiveMiles() {
+//   console.log('Go for a five-mile run');
+// }
 
-  function morningRoutine(exercise) {
-    let breakfast;
-  
-    if (exercise === liftWeights) {
-      breakfast = 'protein bar';
-    } else if (exercise === swimFortyLaps) {
-      breakfast = 'kale smoothie';
-    } else {
-      breakfast = 'granola';
-    }
-  
-    exerciseRoutine(exercise);
-    return function() {
-        console.log(`Nom nom nom, this ${breakfast} is delicious!`);
-      }
-    }
+// function liftWeights() {
+//   console.log('Pump iron');
+// }
 
-    const afterExercise = morningRoutine(liftWeights);
+// function swimFortyLaps() {
+//   console.log('Swim 40 laps');
+// }
 
-    afterExercise;
+// function exerciseRoutine(postRunActivity) {
+//   runFiveMiles();
+//   postRunActivity();
+// }
 
-    afterExercise();
+// function Monday() {
+//   exerciseRoutine(liftWeights);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function receivesAFunction (cb) {
+  return cb();
+}
+
+function namedFunction() {
+  console.log('hello');
+}
+
+function returnsANamedFunction() {
+  return namedFunction;
+}
+
+// const returnsAnAnonymousFunction = function() {
+//   returns function(anonymous);
+// }
